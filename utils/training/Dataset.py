41417d4317b6ec37a8a001ca713cb6a9b6888125
Win <chatcharinsang@gmail.com>
2024-07-15 01:23:30 +0700

    refactor: dataset

--- utils/training/Dataset.py
+++ utils/training/Dataset.py
@@ -1,32 +1,24 @@
-import glob
 import random
-import sys
+from pathlib import Path
 
 import cv2
+import torch
 import torchvision.transforms as transforms
-import tqdm
 from PIL import Image
 from torch.utils.data import TensorDataset
 
-sys.path.append("..")
-# from utils.cap_aug import CAP_AUG
-
 
 class FaceEmbed(TensorDataset):
-    def __init__(self, data_path_list, same_prob=0.8):
-        datasets = []
-        # embeds = []
-        self.N = []
-        self.same_prob = same_prob
-        for data_path in data_path_list:
-            image_list = glob.glob(f"{data_path}/*.*g")
-            datasets.append(image_list)
-            self.N.append(len(image_list))
-            # with open(f'{data_path}/embed.pkl', 'rb') as f:
-            #     embed = pickle.load(f)
-            #     embeds.append(embed)
-        self.datasets = datasets
-        # self.embeds = embeds
+    def __init__(self, image_dir: str, p_return_same_person: float = 0.8) -> None:
+        image_dir: Path = Path(image_dir)
+        # Check if image dir exists
+        if not image_dir.exists():
+            raise FileNotFoundError(f"Image directory {image_dir} not found.")
+
+        # Recursively get all image paths in the directory.
+        self.datasets = list(image_dir.glob("**/*.jpg"))
+        self.p_return_same_person = p_return_same_person
+
         self.transforms_arcface = transforms.Compose(
             [
                 transforms.ColorJitter(0.2, 0.2, 0.2, 0.01),
@@ -45,107 +37,41 @@ class FaceEmbed(TensorDataset):
             ]
         )
 
-    def __getitem__(self, item):
-        idx = 0
-        while item >= self.N[idx]:
-            item -= self.N[idx]
-            idx += 1
+    def __len__(self) -> int:
+        """Return the number of images in the dataset."""
+        return len(self.datasets)
 
-        image_path = self.datasets[idx][item]
-        # name = os.path.split(image_path)[1]
-        # embed = self.embeds[idx][name]
-        Xs = cv2.imread(image_path)[:, :, ::-1]
-        Xs = Image.fromarray(Xs)
-
-        if random.random() > self.same_prob:
-            image_path = random.choice(
-                self.datasets[random.randint(0, len(self.datasets) - 1)]
-            )
-            Xt = cv2.imread(image_path)[:, :, ::-1]
-            Xt = Image.fromarray(Xt)
-            same_person = 0
+    def __getitem__(
+        self, idx: int
+    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, int]:
+        """Get the image tensors and whether they are the same person or not"""
+        # Get the source image
+        source_path = self.datasets[idx]
+        # Read the image
+        source_image = cv2.imread(str(source_path))
+        source_image = cv2.cvtColor(source_image, cv2.COLOR_BGR2RGB)
+        source_image = Image.fromarray(source_image)
+        # Randomly decide if the target image will be the same person or not.
+        same_person = random.random() < self.p_return_same_person
+        if same_person:
+            # Same person
+            target_image = source_image.copy()
         else:
-            Xt = Xs.copy()
-            same_person = 1
+            # Different person
+            target_idx = random.randint(0, len(self.datasets) - 1)
+            # If accidentally picked the same image, pick the next one.
+            if target_idx == idx:
+                target_idx = (target_idx + 1) % len(self.datasets)
+            # Get the target image
+            target_path = self.datasets[target_idx]
+            # Read the image
+            target_image = cv2.imread(str(target_path))
+            target_image = cv2.cvtColor(target_image, cv2.COLOR_BGR2RGB)
+            target_image = Image.fromarray(target_image)
 
         return (
-            self.transforms_arcface(Xs),
-            self.transforms_base(Xs),
-            self.transforms_base(Xt),
-            same_person,
+            self.transforms_arcface(source_image),
+            self.transforms_base(source_image),
+            self.transforms_base(target_image),
+            int(same_person),
         )
-
-    def __len__(self):
-        return sum(self.N)
-
-
-class FaceEmbedVGG2(TensorDataset):
-    def __init__(self, data_path, same_prob=0.8, same_identity=False):
-
-        self.same_prob = same_prob
-        self.same_identity = same_identity
-
-        self.images_list = glob.glob(f"{data_path}/*/*.*g")
-        self.folders_list = glob.glob(f"{data_path}/*")
-
-        self.folder2imgs = {}
-
-        for folder in tqdm.tqdm(self.folders_list):
-            folder_imgs = glob.glob(f"{folder}/*")
-            self.folder2imgs[folder] = folder_imgs
-
-        self.N = len(self.images_list)
-
-        self.transforms_arcface = transforms.Compose(
-            [
-                transforms.ColorJitter(0.2, 0.2, 0.2, 0.01),
-                transforms.Resize((224, 224)),
-                transforms.ToTensor(),
-                # transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
-                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-            ]
-        )
-
-        self.transforms_base = transforms.Compose(
-            [
-                transforms.ColorJitter(0.2, 0.2, 0.2, 0.01),
-                transforms.Resize((256, 256)),
-                transforms.ToTensor(),
-                # transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
-                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-            ]
-        )
-
-    def __getitem__(self, item):
-
-        image_path = self.images_list[item]
-
-        Xs = cv2.imread(image_path)[:, :, ::-1]
-        Xs = Image.fromarray(Xs)
-
-        if self.same_identity:
-            folder_name = "/".join(image_path.split("/")[:-1])
-
-        if random.random() > self.same_prob:
-            image_path = random.choice(self.images_list)
-            Xt = cv2.imread(image_path)[:, :, ::-1]
-            Xt = Image.fromarray(Xt)
-            same_person = 0
-        else:
-            if self.same_identity:
-                image_path = random.choice(self.folder2imgs[folder_name])
-                Xt = cv2.imread(image_path)[:, :, ::-1]
-                Xt = Image.fromarray(Xt)
-            else:
-                Xt = Xs.copy()
-            same_person = 1
-
-        return (
-            self.transforms_arcface(Xs),
-            self.transforms_base(Xs),
-            self.transforms_base(Xt),
-            same_person,
-        )
-
-    def __len__(self):
-        return self.N
