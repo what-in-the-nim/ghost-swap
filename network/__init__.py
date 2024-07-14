18ba8e73cd391e3b61d8d6be2003431aed1b3fdb
Win <chatcharinsang@gmail.com>
2024-07-15 02:40:58 +0700

    style

--- network/__init__.py
+++ network/__init__.py
@@ -2,4 +2,4 @@ from .AEI_Net import AEI_Net
 from .fan import FAN
 from .multi_scale_discriminator import MultiscaleDiscriminator
 
-__all__ = ["AEI_Net","FAN", "MultiscaleDiscriminator"]
+__all__ = ["AEI_Net", "FAN", "MultiscaleDiscriminator"]
--- network/attribute_encoder/multi_attribute_encoder_resnet.py
+++ network/attribute_encoder/multi_attribute_encoder_resnet.py
@@ -130,7 +130,7 @@ class MLAttrEncoderResnet(nn.Module):
         layers = []
         layers.append(block(self.inplanes, planes, stride, downsample))
         self.inplanes = planes * block.expansion
-        for i in range(1, blocks):
+        for _ in range(1, blocks):
             layers.append(block(self.inplanes, planes))
 
         return nn.Sequential(*layers)
