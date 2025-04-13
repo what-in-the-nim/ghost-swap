29e9f6c568e80f33288c9ee894ea2fa17305428f
what-in-the-nim <chatcharinsang@gmail.com>
2025-04-13 20:06:59 +0700

    feat: add iresnet

--- download_models.sh
+++ download_models.sh
@@ -1,6 +1,5 @@
 # load arcface
 wget -P ./arcface_model https://github.com/sberbank-ai/sber-swap/releases/download/arcface/backbone.pth
-wget -P ./arcface_model https://github.com/sberbank-ai/sber-swap/releases/download/arcface/iresnet.py
 
 # load landmarks detector
 wget -P ./insightface_func/models/antelope https://github.com/sberbank-ai/sber-swap/releases/download/antelope/glintr100.onnx
